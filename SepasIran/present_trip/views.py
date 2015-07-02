5607a89cdbb381b30e9278aae637e9edf28953f4
fartahouni <farzanehtahooni@FT.local>
2015-07-02 18:49:58 +0430

    search/result/<username>/pack --> fixed.

--- SepasIran/present_trip/views.py
+++ SepasIran/present_trip/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 def home(request , username = ''):
     return render(request, 'present_trip/home.html', {'username':username})
 
-def show_one_trip(request, kind = '' ,tour_id = '1'):
+def show_one_trip(request, kind = '' ,username = ''):
     if kind == 'service':
-        return render(request , "present_trip/one_trip.html", {'kind':kind})
+        return render(request , "present_trip/one_trip.html", {'kind':kind , 'username':username})
     elif kind == 'tour':
-        return render(request , "present_trip/one_trip.html", {'kind':kind})
+        return render(request , "present_trip/one_trip.html", {'kind':kind , 'username':username})
     elif kind == 'pack':
-        return render(request , "present_trip/one_trip.html", {'kind':kind})
+        return render(request , "present_trip/one_trip.html", {'kind':kind , 'username':username})
 
 def show_one_trip_status(request , kind =''):
     return render(request , "present_trip/status_trip.html")
 
-def search(request):
-    return render(request , "present_trip/search_result.html")
+def search(request , username = '' , ispack = ''):
+
+    return render(request , "present_trip/search_result.html" , {'username':username , 'ispack':ispack})
 # Create your views here.
