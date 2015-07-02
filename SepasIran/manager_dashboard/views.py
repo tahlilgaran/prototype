8c7f701869ff832195147f416b7ece402491a7ce
fartahouni <farzanehtahooni@FT.local>
2015-07-03 01:05:09 +0430

    Revert "ویوی نظرات آنلاین و اطلاعات گردشگران"
    Conflicts:
    	SepasIran/SepasIran/urls.py
    	SepasIran/templates/base.html
    	SepasIran/tourism/__pycache__/__init__.cpython-34.pyc
    	SepasIran/tourism/__pycache__/admin.cpython-34.pyc
    	SepasIran/tourism/__pycache__/models.cpython-34.pyc
    	SepasIran/tourism/migrations/__pycache__/__init__.cpython-34.pyc
    	SepasIran/user/__pycache__/__init__.cpython-34.pyc
    	SepasIran/user/__pycache__/admin.cpython-34.pyc
    	SepasIran/user/__pycache__/models.cpython-34.pyc
    	SepasIran/user/migrations/__pycache__/__init__.cpython-34.pyc

--- SepasIran/manager_dashboard/views.py
+++ SepasIran/manager_dashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Dashboard(request):
+    return render(request,"manager_dashboard.html",{"username" : "admin"})
+
+
+def tourLists(request):
+     return render(request,"manager_tours.html",{"username" : "admin"})
+
+def tourRating(request):
+    return render(request,"manager_tours_rating.html",{"username" : "admin"})
