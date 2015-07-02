f7ba8ab0ffed6fd1fc05176a968e53860ec14ca5
nmahzoon <niloofarmahzoon@gmail.com>
2015-07-02 20:00:38 +0430

    define tour complete

--- SepasIran/define_and_validate_trip/views.py
+++ SepasIran/define_and_validate_trip/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 def tarif_kind(request,username):
 
+    if request.POST.get("next","") != "":
+        print("next")
+        tour = request.POST.getlist("optradio")
+        if tour:
+            print("aval")
+        if 1 in tour:
+            print("write")
+            return HttpResponseRedirect("/home/")
+
     return  render(request,"tarif_kind.html",{
+         'username': username,
+    })
+
+
+def tour_define(request,username):
+
+    return render(request,"tour_define.html",{
         'username': username,
     })
